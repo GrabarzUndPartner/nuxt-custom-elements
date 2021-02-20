cd777daf5c64ba4836da70d1f65172c0f174d6ac
Thorn Walli <thorn.walli@grabarzundpartner.de>
2021-02-20 22:13:45 +0100

    fix(sonar): fix songar bugs

--- tools/express-server.js
+++ tools/express-server.js
@@ -1,6 +1,7 @@
 const { paramCase } = require('change-case');
 const express = require('express');
 const app = express();
+app.disable('x-powered-by');
 
 const dist = getDist();
 const host = getHost();
