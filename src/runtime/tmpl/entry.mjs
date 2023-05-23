286e881ff40189eeb26c03480c665a6159f879e5
Zack Spear <hi@zackspear.com>
2023-05-23 14:20:21 -0700

    fix(entries): multiple entries non-asynn

--- src/runtime/tmpl/entry.mjs
+++ src/runtime/tmpl/entry.mjs
@@ -1,16 +1,18 @@
-
 import { defineAsyncComponent, defineCustomElement } from 'vue'
 
 <%= options.tags.filter(function ({ async }) { return !async; }).map(({ path }, i) => `import Component${i} from '${path}';`).join('\n') %>
 
 const defineTags = () => {
   const elements = [
-<%= options.tags.map(function ({ async, name, path }, i) {
+<% let i = 0; %><%= options.tags.map(function ({ async, name, path }) {
+  let tag;
   if (async) {
-    return`    ['${name}', defineAsyncComponent(() => { return import('${path}').then(module => (typeof module.default === 'function' ? (new module.default).$options : module.default) ); })]`;
+    tag = `    ['${name}', defineAsyncComponent(() => { return import('${path}').then(module => (typeof module.default === 'function' ? (new module.default).$options : module.default) ); })]`;
   } else {
-    return`    ['${name}', (typeof Component${i} === 'function' ? (new Component${i}).$options : Component${i})]`;
+    tag = `    ['${name}', (typeof Component${i} === 'function' ? (new Component${i}).$options : Component${i})]`;
+    i++;
   }
+  return tag;
  }).join(',\n') %>
   ].forEach(([name, component]) => {
     const CustomElement = defineCustomElement(component);
